0ccad885e94d00bd336ee5d082da3a2ee1d3b7e1
Devmustroc <elmustapha.abourar@outlook.com>
2023-11-01 15:25:48 +0100

    10. Longest Palindrome

--- 0x01-dart-function/10-longest-palindrome.dart
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -1,31 +1,30 @@
+bool isPalindrome(String s) {
+  for (int i = 0; i < s.length ~/ 2; i++) {
+    if (s[i] != s[s.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
 String longestPalindrome(String s) {
-  bool isPalindrome(String str) {
-    int start = 0;
-    int end = str.length - 1;
-
-    while (start < end) {
-      if (str[start] != str[end]) {
-        return false;
-      }
-      start++;
-      end--;
-    }
-    return true;
+  if (s.length < 3) {
+    return "none";
   }
 
-  int maxLength = 0;
   String longest = "none";
-  if (s.length < 2) {
-    return s;
-  }
   for (int i = 0; i < s.length; i++) {
-    for (int j = i + 1; j <= s.length; j++) {
-      String sub = s.substring(i, j);
-      if (isPalindrome(sub) && sub.length > maxLength) {
-        maxLength = sub.length;
-        longest = sub;
+    for (int j = i + 2; j <= s.length; j++) {
+      String substring = s.substring(i, j);
+      if (isPalindrome(substring) && substring.length > longest.length) {
+        longest = substring;
       }
     }
   }
+
+  if (longest == "none") {
+    return "none";
+  }
+
   return longest;
 }
